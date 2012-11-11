92dd2aae3e3001d0eced47fd805f013597ff6783
Chi-kwan Chan <ckch@nordita.org>
2012-11-10 20:54:31 -0800

    Start working on the general relativistic geodesic integrator

---
+++ src/gr/state.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2012 Chi-kwan Chan
+// Copyright (C) 2012 Steward Observatory
+//
+// This file is part of geode.
+//
+// Geode is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Geode is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with geode.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef STATE_HPP
+#define STATE_HPP
+
+typedef struct {
+  Real t, r, theta, phi;
+  Real kr, ktheta;
+  Real bimpact; // impact parameter defined as L / E
+} State;
+
+#endif // STATE_HPP
