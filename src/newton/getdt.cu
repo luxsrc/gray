cc8665959908667237fa830ce66e6df0b8cb2c45
Chi-kwan Chan <ckch@nordita.org>
2012-11-13 21:17:54 -0800

    Add getdt() to "newton" so `make newton` works

---
+++ src/newton/getdt.cu
@@ -0,0 +1,22 @@
+// Copyright (C) 2012 Chi-kwan Chan
+// Copyright (C) 2012 Steward Observatory
+//
+// This file is part of geode.
+//
+// Geode is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Geode is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+// License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with geode.  If not, see <http://www.gnu.org/licenses/>.
+
+static __device__ Real getdt(const Var v, const State a)
+{
+  return 1.0e-3;
+}
