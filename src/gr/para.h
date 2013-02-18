2eba1a40cdbbc20da248363ec3ec004736b202cd
Chi-kwan Chan <chanc@email.arizona.edu>
2013-02-17 16:24:31 -0800

    Numerical difficulty at a=0 is caused by photons passing event horizon

--- src/gr/para.h
+++ src/gr/para.h
@@ -23,11 +23,11 @@
 #  define N_DEFAULT (180 * 600)
 #  define DT_DUMP   (-100)
 #else // for src/init.cu and src/evolve.cu
-static __constant__ real r_obs    = 1000;       // observer radius in GM/c^2
-static __constant__ real i_obs    = 30;         // observer theta in degrees
-static __constant__ real a_spin   = 0.999;      // dimensionless spin j/mc
-static __constant__ real dt_scale = 1.0 / 1024; // typical step size
-static __constant__ real epsilon  = 1e-6;       // stop photon
+static __constant__ real r_obs    = 1000;      // observer radius in GM/c^2
+static __constant__ real i_obs    = 30;        // observer theta in degrees
+static __constant__ real a_spin   = 0.999;     // dimensionless spin j/mc
+static __constant__ real dt_scale = 1.0 / 256; // typical step size
+static __constant__ real epsilon  = 1e-6;      // stop photon
 
 static inline bool config(const char c, const real v)
 {
