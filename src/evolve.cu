033735ff1150f2ff7c78400404c1d087394c7e1a
Chi-kwan Chan <chanc@email.arizona.edu>
2013-03-12 00:36:54 -0700

    Update copyright years to include 2013

--- src/evolve.cu
+++ src/evolve.cu
@@ -1,5 +1,5 @@
-// Copyright (C) 2012 Chi-kwan Chan
-// Copyright (C) 2012 Steward Observatory
+// Copyright (C) 2012,2013 Chi-kwan Chan
+// Copyright (C) 2012,2013 Steward Observatory
 //
 // This file is part of GRay.
 //
