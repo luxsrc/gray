773fa75c1bae8bc2262cb2e4658fc338f8815226
Chi-kwan Chan <chanc@email.arizona.edu>
2013-03-15 17:49:21 -0700

    Draw the controller as a stick man

--- src/sense.cc
+++ src/sense.cc
@@ -17,12 +17,13 @@
 // along with GRay.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "gray.h"
+#include <cmath>
 #include <NiTE.h>
 
 static bool first = true;
 
 static nite::UserTracker tracker;
-static nite::Point3f head;
+static nite::Point3f head, neck, torso, upper[6], lower[6];
 
 static void setup()
 {
@@ -52,17 +53,38 @@ void sense()
   if(nite::STATUS_OK == tracker.readFrame(&frame)) {
     const nite::Array<nite::UserData>& users = frame.getUsers();
 
-    head.z = 0;
     for(int i = 0; i < users.getSize(); ++i) {
-      const nite::UserData& user = users[i];
+      const nite::UserData& u = users[i];
 
-      if(user.isNew())
-        tracker.startSkeletonTracking(user.getId());
-      else if(nite::SKELETON_TRACKED == user.getSkeleton().getState()) {
-        head = user.getSkeleton().getJoint(nite::JOINT_HEAD).getPosition();
-        print("head: (%g, %g, %g)\n", head.x, head.y, head.z);
+      if(u.isNew())
+        tracker.startSkeletonTracking(u.getId());
+      else if(nite::SKELETON_TRACKED == u.getSkeleton().getState()) {
+        const nite::Point3f h =
+          u.getSkeleton().getJoint(nite::JOINT_HEAD).getPosition();
+        print("head: (%g, %g, %g)\n", h.x, h.y, h.z);
       }
     }
+
+    if(users.getSize() > 0) {
+      const nite::Skeleton& s = users[0].getSkeleton();
+
+      head     = s.getJoint(nite::JOINT_HEAD          ).getPosition();
+      neck     = s.getJoint(nite::JOINT_NECK          ).getPosition();
+      torso    = s.getJoint(nite::JOINT_TORSO         ).getPosition();
+      upper[0] = s.getJoint(nite::JOINT_LEFT_HAND     ).getPosition();
+      upper[1] = s.getJoint(nite::JOINT_LEFT_ELBOW    ).getPosition();
+      upper[2] = s.getJoint(nite::JOINT_LEFT_SHOULDER ).getPosition();
+      upper[3] = s.getJoint(nite::JOINT_RIGHT_SHOULDER).getPosition();
+      upper[4] = s.getJoint(nite::JOINT_RIGHT_ELBOW   ).getPosition();
+      upper[5] = s.getJoint(nite::JOINT_RIGHT_HAND    ).getPosition();
+      lower[0] = s.getJoint(nite::JOINT_LEFT_FOOT     ).getPosition();
+      lower[1] = s.getJoint(nite::JOINT_LEFT_KNEE     ).getPosition();
+      lower[2] = s.getJoint(nite::JOINT_LEFT_HIP      ).getPosition();
+      lower[3] = s.getJoint(nite::JOINT_RIGHT_HIP     ).getPosition();
+      lower[4] = s.getJoint(nite::JOINT_RIGHT_KNEE    ).getPosition();
+      lower[5] = s.getJoint(nite::JOINT_RIGHT_FOOT    ).getPosition();
+    } else
+      head.z = 0;
   }
 }
 
@@ -72,11 +94,37 @@ void track()
   glTranslatef(0, 0, -5); // translate 5 meters
 
   if(head.z != 0) {
-    glPushMatrix();
-    glTranslatef(head.x / 1000, head.y / 1000, head.z / 1000);
-    glRotatef(-90, 1, 0, 0);
     glColor3f(1, 1, 0);
-    glutSolidSphere(0.1, 32, 16);
-    glPopMatrix();
+
+    glBegin(GL_LINE_STRIP);
+    const float hx = head.x / 1000, hy = head.y / 1000, hz = head.z / 1000;
+    for(int i = -16; i <= 48; ++i) {
+      const float phi = M_PI * i / 32;
+      glVertex3f(hx + 0.1 * cos(phi), hy + 0.1 * sin(phi), hz);
+    }
+    glVertex3f(neck.x / 1000, neck.y / 1000, neck.z / 1000);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    glVertex3f(upper[2].x / 1000, upper[2].y / 1000, upper[2].z / 1000);
+    glVertex3f(   torso.x / 1000,    torso.y / 1000,    torso.z / 1000);
+    glVertex3f(upper[3].x / 1000, upper[3].y / 1000, upper[3].z / 1000);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    glVertex3f(lower[2].x / 1000, lower[2].y / 1000, lower[2].z / 1000);
+    glVertex3f(   torso.x / 1000,    torso.y / 1000,    torso.z / 1000);
+    glVertex3f(lower[3].x / 1000, lower[3].y / 1000, lower[3].z / 1000);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    for(int i = 0; i < 6; ++i)
+      glVertex3f(upper[i].x / 1000, upper[i].y / 1000, upper[i].z / 1000);
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    for(int i = 0; i < 6; ++i)
+      glVertex3f(lower[i].x / 1000, lower[i].y / 1000, lower[i].z / 1000);
+    glEnd();
   }
 }
