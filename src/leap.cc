186ecb5765caefeec589ff1d7a1c2568b4ad8506
Chi-kwan Chan <chanc@email.arizona.edu>
2014-03-03 16:15:07 -0500

    Reimplement gestures control

--- src/leap.cc
+++ src/leap.cc
@@ -34,88 +34,88 @@ void GRayLeapListener::onConnect(const Leap::Controller& controller)
 
 void GRayLeapListener::onFrame(const Leap::Controller& controller)
 {
-  // Get the most recent frame and report some basic information
-  const Leap::Frame frame = controller.frame();
+  static int type = 0;
+  static float d_old, x_old, z_old, ax_old, ly_old, az_old;
 
-  const Leap::GestureList gestures = frame.gestures();
-  const Leap::HandList    hands    = frame.hands();
+  const Leap::Frame    frame = controller.frame();
+  const Leap::HandList hands = frame.hands();
 
-  if (!gestures.isEmpty()) {
-    Leap::Gesture gesture = gestures[gestures.count()-1];
+  if(hands.count() == 2) {
+    const Leap::FingerList lfingers = hands[0].fingers();
+    const Leap::FingerList rfingers = hands[1].fingers();
+    Leap::Vector lpos, rpos;
 
-    switch (gesture.type()) {
-    case Leap::Gesture::TYPE_CIRCLE:
-      print("Circle gesture\n");
-      {
-	Leap::CircleGesture circle = gesture;
+    for(int i = 0; i < lfingers.count(); ++i)
+      lpos += lfingers[i].tipPosition();
+    lpos /= (float)lfingers.count();
 
-        if (circle.pointable().direction().angleTo(circle.normal())
-            <= Leap::PI/4) {
-          if (global::dt_dump != 0.0)
-            global::dt_dump  = -fabs(global::dt_dump);
-          else {
-            global::dt_dump  = -fabs(global::dt_saved);
-            global::dt_saved = 0.0;
-	  }
-        } else {
-          if (global::dt_dump != 0.0)
-            global::dt_dump  = +fabs(global::dt_dump);
-          else {
-            global::dt_dump  = +fabs(global::dt_saved);
-            global::dt_saved = 0.0;
-	  }
-        }
+    for(int i = 0; i < rfingers.count(); ++i)
+      rpos += rfingers[i].tipPosition();
+    rpos /= (float)rfingers.count();
+
+    const float d = lpos.distanceTo(rpos);
+    if(d > 10.0) { // d may be NaN
+      if(type != 2) {
+        d_old  = d;
+        ly_old = global::ly;
+        type   = 2;
       }
-      break;
-    default:
-      print("Unknown gesture type\n");
-      break;
+      global::ly = ly_old * d_old / d;
     }
-  } else if (!hands.isEmpty()) {
-    static int   left = 0, right = 0;
-    static float d_old, x_old, z_old, ax_old, ly_old, az_old;
+  } else if(hands.count() == 1 && hands[0].fingers().count() >= 4) {
+    const Leap::FingerList fingers = hands[0].fingers();
+    Leap::Vector pos;
 
-    if (hands.count() == 1) {
-      const Leap::FingerList fingers = hands[0].fingers();
-      if (fingers.count() == 5) {
-	print("Pause\n");
-	if (global::dt_dump != 0.0) {
-          global::dt_saved = global::dt_dump;
-          global::dt_dump  = 0.0;
-	}
-        left = right = 0;
-      } else if (fingers.count() == 1) {
-        print("Rotating\n");
-        Leap::Vector pos = fingers[0].tipPosition();
-        if (!right) {
-          ax_old = global::ax;
-          az_old = global::az;
-          x_old = -pos.x;
-          z_old =  pos.y;
-        }
-        global::ax = ax_old + (-pos.x - x_old) / 2;
-        global::az = az_old + ( pos.y - z_old) / 2;
-        right = 1;
-      } else
-        left = right = 0;
-    } else if(hands.count() == 2) {
-      const Leap::FingerList lfingers = hands[0].fingers();
-      const Leap::FingerList rfingers = hands[1].fingers();
-      if (lfingers.count() == 1 && rfingers.count() == 1) {
-        print("Zooming\n");
-        const float d =
-          lfingers[0].tipPosition().distanceTo(rfingers[0].tipPosition());
-        if (!left || !right) {
-          d_old  = d;
-          ly_old = global::ly;
-        }
-        global::ly = ly_old * d_old / d;
-        left = right = 1;
-      } else
-        left = right = 0;
-    } else
-      left = right = 0;
-  }
+    for(int i = 0; i < fingers.count(); ++i)
+      pos += fingers[i].tipPosition();
+    pos /= (float)fingers.count();
+
+    if(type != 1) {
+      ax_old = global::ax;
+      az_old = global::az;
+      x_old  = -pos.x;
+      z_old  =  pos.y;
+      type   = 1;
+    }
+    global::ax = ax_old + (-pos.x - x_old) / 2;
+    global::az = az_old + ( pos.y - z_old) / 2;
+  } else if(hands.count() == 1 && hands[0].fingers().count() <= 1) {
+    // Pause or run the simulation
+    const Leap::GestureList gestures = frame.gestures();
+    int direction = 0;
+
+    for(int i = 0; i< gestures.count(); ++i)
+      if(gestures[i].type() == Leap::Gesture::TYPE_CIRCLE) {
+	Leap::CircleGesture circle = gestures[i];
+	direction = circle.pointable().direction().angleTo(circle.normal())
+                 <= Leap::PI/4 ? 1 : -1;
+	break;
+      }
+
+    switch(direction) {
+    case -1: if(global::dt_dump != 0.0)
+	       global::dt_dump  = +fabs(global::dt_dump);
+	     else {
+	       global::dt_dump  = +fabs(global::dt_saved);
+	       global::dt_saved = 0.0;
+	     }
+	     break;
+    case  0: if(global::dt_dump != 0.0) {
+               global::dt_saved = global::dt_dump;
+               global::dt_dump  = 0.0;
+	     }
+ 	     break;
+    case  1: if(global::dt_dump != 0.0)
+	       global::dt_dump  = -fabs(global::dt_dump);
+	     else {
+	       global::dt_dump  = -fabs(global::dt_saved);
+	       global::dt_saved = 0.0;
+	     }
+	     break;
+    }
+    type = 0;
+  } else
+    type = 0;
 }
 
 static Leap::Controller *controller = NULL;
