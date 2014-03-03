888e08f03e29afcdd13000c7883c8c5c181583bd
Chi-kwan Chan <chanc@email.arizona.edu>
2014-03-03 13:15:09 -0500

    Add a simple listener; the tap gesture pauses and resumes calculations

--- src/leap.cc
+++ src/leap.cc
@@ -21,18 +21,58 @@
 #ifndef DISABLE_LEAP
 #include <Leap.h>
 
+class GRayLeapListener : public Leap::Listener {
+  public:
+    virtual void onConnect(const Leap::Controller&);
+    virtual void onFrame  (const Leap::Controller&);
+};
+
+void GRayLeapListener::onConnect(const Leap::Controller& controller)
+{
+  controller.enableGesture(Leap::Gesture::TYPE_KEY_TAP);
+}
+
+void GRayLeapListener::onFrame(const Leap::Controller& controller)
+{
+  // Get the most recent frame and report some basic information
+  const Leap::Frame frame = controller.frame();
+
+  // Get gestures
+  const Leap::GestureList gestures = frame.gestures();
+  for (int g = 0; g < gestures.count(); ++g) {
+    Leap::Gesture gesture = gestures[g];
+
+    switch (gesture.type()) {
+    case Leap::Gesture::TYPE_KEY_TAP:
+      {
+        const double temp = global::dt_saved;
+        global::dt_saved = global::dt_dump;
+        global::dt_dump = temp;
+        break;
+      }
+    default:
+        print("Unknown gesture type.\n");
+        break;
+    }
+  }
+}
+
 static Leap::Controller *controller = NULL;
+static GRayLeapListener *listener   = NULL;
 
 static void setup()
 {
   print("Leaping motion...");
   controller = new Leap::Controller;
+  listener   = new GRayLeapListener;
+  controller->addListener(*listener);
   print(" DONE\n");
 }
 
 static void cleanup()
 {
   print("Cleanup...");
+  controller->removeListener(*listener);
   print(" DONE\n");
 }
 
