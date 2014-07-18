5b12a5bbf8c0ff6bd8a5cd8fda053f88c1454147
Chi-kwan Chan <chanc@email.arizona.edu>
2014-07-18 19:23:40 -0400

    Turn off extrapolation by default

--- src/Kerr/const.h
+++ src/Kerr/const.h
@@ -22,7 +22,7 @@
 #include "harm.h"
 
 #define DT_DUMP (-100)
-#define N_RX    139 // use power-law extrapolation for r > c.r[N_RX]
+//efine N_RX    139 // use power-law extrapolation for r > c.r[N_RX]
 #define N_R     264 // large enough to hold our grids
 //efine N_THETA 128 // large enough to hold out grids
 //efine N_IN    64  // inner theta-grid is stored in constant memory
