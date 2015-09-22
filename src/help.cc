439b9220d9e714df182c5e805ed2a846d3ad0c61
Chi-kwan Chan <chanc@email.arizona.edu>
2015-09-22 11:19:02 -0700

    List the most useful parameters

--- src/help.cc
+++ src/help.cc
@@ -20,7 +20,35 @@
 
 int help(const char *argv0)
 {
-  print("usage: %s [--help] [para=val...]\n", argv0);
+  print("usage: %s [OPTION] [PARAMETER=VALUE ...]\n", argv0);
+  print("\n\
+Available OPTION includes:\n\
+     --help  display this help and exit\n\
+\n\
+Available PARAMETER includes:\n\
+     gpu    gpu id for running the job\n\
+     n      total number of rays\n\
+     t0     start time\n\
+     dt     separation time between each snapshot\n\
+     imgs   name of the output file that stores images\n\
+     rays   name of the output file that stores full rays\n\
+     grid   name of the output file that stores the source grid\n\
+\n\
+     imgsz  size of the computed image in GM/c^2\n\
+     imgx0  horizontal coordinate of the image center\n\
+     imgy0  vertical coordinate of the image center \n\
+     r      distance between the image plane and the black hole\n\
+     i      inclination angle in degrees\n\
+     j      azimuthal angle in degrees\n\
+     ne     normalization for the electron number density\n\
+     beta   threshold plasma beta to distinguish the disk and funnel\n\
+     td     electron-ion temperature ratio for the disk\n\
+     tf     electron-ion temperature ratio for the funnel\n\
+     Tf     constant temperature for the funnel\n\
+     harm   snapshots of GRMHD simulations created by HARM\n\
+     nu     frequencies in Hz; can be a list of numbers or a file name\n\
+\n");
+  print("Report bugs to <chanchikwan@gmail.com>.\n");
 
   return 0;
 }
