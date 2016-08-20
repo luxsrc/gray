f3dc036af27a0696da435de789d8336d8d5c9479
Chi-kwan Chan <chanc@email.arizona.edu>
2016-08-19 20:19:35 -0700

    A minimal lux job

---
+++ sim/gray.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2016 Chi-kwan Chan
+ * Copyright (C) 2016 Steward Observatory
+ *
+ * This file is part of GRay2.
+ *
+ * GRay2 is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GRay2 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GRay2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _GRAY_H_
+#define _GRAY_H_
+
+#include <lux/job.h>
+
+struct gray {
+	Lux_job super;
+};
+
+#endif /* _GRAY_H */
