4610a2688c45e13ea3a01797e92f6a0ea1c53172
Chi-kwan Chan <chanc@email.arizona.edu>
2016-08-21 12:10:25 -0700

    Document the overall header "sim/gray.h"

--- sim/gray.h
+++ sim/gray.h
@@ -17,6 +17,16 @@
  * You should have received a copy of the GNU General Public License
  * along with GRay2.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/** \file
+ ** Data structure definitions and function declarations for GRay2
+ **
+ ** GRay2 is implemented as a lux module.  Its run-time data is stored
+ ** in a subclass of Lux_job, which is defined in this header file.
+ ** Additional structure that holds run-time adjustable parameters,
+ ** constructor, destructor, internal functions, and standard methods
+ ** in Lux_job, are all declared here as well.
+ **/
 #ifndef _GRAY_H_
 #define _GRAY_H_
 
@@ -28,6 +38,14 @@
 #include "param.h"
 #include "setup.h"
 
+/**
+ ** Run-time data for GRay2
+ **
+ ** To take advantage of all the low level features provided by lux,
+ ** GRay2 is implemented as a lux module.  Its runtime data is stored
+ ** in a subclass of Lux_job so that it can be loaded by the lux
+ ** runtime.
+ **/
 struct gray {
 	Lux_job      super;
 	struct icond icond;
@@ -42,9 +60,16 @@ struct gray {
 #define EGO ((struct gray *)ego)
 #define CKR lux_check_func_success
 
+/** Configuration method in the Lux_Job interface */
 extern int  conf(Lux_job *, const char *);
+
+/** Initialization method in the Lux_Job interface */
 extern int  init(Lux_job *);
+
+/** Execution method in the Lux_Job interface */
 extern int  exec(Lux_job *);
+
+/** Output function */
 extern void dump(Lux_job *, const char *);
 
 #endif /* _GRAY_H */
