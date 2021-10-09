93771740a150949bc844f3cf98188ccf12350f06
Chi-kwan Chan <chanc@arizona.edu>
2021-10-09 00:19:25 -0700

    Use a module design so `gray` is divided into `gi`, `flow`, and `rt`

---
+++ mod/gray.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2021 Gabriele Bozzola and Chi-kwan Chan
+ * Copyright (C) 2021 Steward Observatory
+ *
+ * This file is part of GRay2.
+ *
+ * GRay2 is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GRay2 is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GRay2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gray.h"
+
+#include <lux/mangle.h>
+#include <lux/zalloc.h>
+
+#define EGO ((struct gray *)ego)
+
+static int
+conf(Lux_job *ego, const char *restrict arg)
+{
+	lux_debug("GRay2: configuring instance %p with \"%s\"\n", ego, arg);
+
+	return 0;
+}
+
+static int
+init(Lux_job *ego)
+{
+	lux_debug("GRay2: initializing instance %p\n", ego);
+
+	return 0;
+}
+
+static int
+exec(Lux_job *ego)
+{
+	lux_debug("GRay2: executing instance %p\n", ego);
+
+	return 0;
+}
+
+void *
+LUX_MKMOD(const void *opts)
+{
+	void *ego;
+
+	lux_debug("GRay2: constructing an instance with options %p\n", opts);
+
+	ego = zalloc(sizeof(struct gray));
+	if(ego) {
+		EGO->super.conf = conf;
+		EGO->super.init = init;
+		EGO->super.exec = exec;
+	}
+	return ego;
+}
+
+void
+LUX_RMMOD(void *ego)
+{
+	lux_debug("GRay2: destructing instance %p\n", ego);
+
+	free(ego);
+}
